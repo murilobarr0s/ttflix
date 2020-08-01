82ba6eec4b32d25f44cb239e8e5ea5022d37fd3d
Murilo Barros <muriloxd02@gmail.com>
2020-08-01 16:25:54 -0300

    aula-5-finalizada

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import useForm from '../../../hocks/useForm';
 import Button from '../../../components/Button';
 import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
   const { handleChange, values } = useForm({
     titulo: 'Video padrão',
     url: 'https://www.youtube.com/watch?v=hhQ3RtvmfEg',
     categoria: 'Tribute',
   });
 
+  useEffect(() => {
+    categoriasRepository
+      .getAll()
+      .then((categoriasFromServer) => {
+        setCategorias(categoriasFromServer);
+      });
+  }, []); // [] -> para nao mandar várias requisições p/ o servidor
+
+  console.log(categorias);
   return (
     <PageDefault>
       <h1>Add Video</h1>
@@ -21,10 +32,14 @@ function CadastroVideo() {
       <form onSubmit={(event) => {
         event.preventDefault();
 
+       const categoriaEscolhida = categorias.find((categoria)=>{
+          return categoria.titulo === values.categoria;
+       });
+
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
-          categoriaId: 1,
+          categoriaId : categoriaEscolhida.id,
         })
 
           .then(() => {
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,6 +1,18 @@
 import config from '../config';
 
 const URL_CATEGORIES = `${config.URL}/categorias`;
+
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        const resposta = await respostaDoServer.json();
+        return resposta;
+      }
+      throw new Error('Não foi possível pegar os dados');
+    });
+}
+
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (respostaDoServer) => {
@@ -13,5 +25,5 @@ function getAllWithVideos() {
 }
 
 export default {
-  getAllWithVideos,
+  getAllWithVideos, getAll
 };
